54ef991ad20484fe27f8f03fb484755f3f024803
Raihane Akkache <raihane.akkache@gmail.com>
2021-01-18 11:35:10 +0100

    design Details Screen

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,7 +1,20 @@
 import React from 'react'
-import { View, Text, Image, ScrollView } from 'react-native'
+import { View, Text, Image, ScrollView, StyleSheet } from 'react-native'
 
+import Dimensions from "../constants/Dimensions";
 import Container from '../components/Container'
+import { capitalizeString } from '../helper';
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 24,
+        fontWeight: "bold"
+    },
+    image: {
+        width: Dimensions.window.width,
+        aspectRatio: 1
+    },
+});
 
 function DetailsScreen({ route }) {
     const { id, title, url } = route.params;
@@ -10,14 +23,14 @@ function DetailsScreen({ route }) {
         <Container flex stretch>
             <ScrollView>
                 <Image
-                    style={{ height: 600 }}
+
+                    style={styles.image}
                     source={{ uri: url }}
+                    //resizeMode="contain"
                     onError={({ nativeEvent: { error } }) => console.log("err deital", error)}
                 />
                 <Container flex stretch padding>
-                    <Text>Details</Text>
-                    <Text>id : {id}</Text>
-                    <Text>title : {title}</Text>
+                    <Text style={styles.title}>{capitalizeString(title)}</Text>
                 </Container>
             </ScrollView>
         </Container>)
