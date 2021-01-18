5f18cda2999d01c0f099a7626e89c1e1b434f5ea
Raihane Akkache <raihane.akkache@gmail.com>
2021-01-18 07:05:25 +0100

    add constants Dimensions and Colors

---
+++ src/constants/Colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    skyblue: '#00BBEE',
+    green: '#00BE11',
+    blue: '#3f37c9',
+    orange: '#fb8500',
+    purple: '#7209b7',
+    red: '#d00000',
+  };
+
+export {colors};
---
+++ src/store/Action.js
@@ -0,0 +1,9 @@
+import { GET_ALBUMS, GET_PHOTOS } from './Reducer'
+
+export const getAlbumsAction = () => ({
+    type: GET_ALBUMS,
+})
+
+export const getPhotosAction = () => ({
+    type: GET_PHOTOS,
+})
